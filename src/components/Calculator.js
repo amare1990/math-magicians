de4f622bcfe99df912d529d48a7680e718611c59
amare1990 <amaremek@gmail.com>
2022-11-02 15:08:48 +0300

    Modify the Calculator.js file to stick to the wireframe design

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -5,34 +5,26 @@ class Calculator {
   calcStr = `
     <div class="calc-wrapper">
       <input type = "text" class ="inp-display" placeholder = 0>
-      <div class = "row first-row">
-        <div class ="grid"> AC </div>
-        <div class ="grid"> +/- </div>
-        <div class ="grid"> % </div>
-        <div class ="grid"> + </div>
-      </div>
-      <div class = "row second-row">
-        <div class ="grid"> 7 </div>
-        <div class ="grid"> 8 </div>
-        <div class ="grid"> 9 </div>
-        <div class ="grid"> * </div>
-      </div>
-      <div class = "row third-row">
-        <div class ="grid"> 4 </div>
-        <div class ="grid"> 5 </div>
-        <div class ="grid"> 6 </div>
-        <div class ="grid"> - </div>
-      </div>
-      <div class = "row fourth-row">
-        <div class ="grid"> 1 </div>
-        <div class ="grid"> 2 </div>
-        <div class ="grid"> 3 </div>
-        <div class ="grid"> + </div>
-      </div>
-      <div class = "row fourth-row">
-        <div class ="grid"> 0 </div>
-        <div class ="grid"> . </div>
-        <div class ="grid"> = </div>
+      <div class = "grid">
+        <div class ="calc-btn"> AC </div>
+        <div class ="calc-btn"> +/- </div>
+        <div class ="calc-btn"> % </div>
+        <div class ="calc-btn op"> + </div>
+        <div class ="calc-btn"> 7 </div>
+        <div class ="calc-btn"> 8 </div>
+        <div class ="calc-btn"> 9 </div>
+        <div class ="calc-btn op"> * </div>
+        <div class ="calc-btn"> 4 </div>
+        <div class ="calc-btn"> 5 </div>
+        <div class ="calc-btn"> 6 </div>
+        <div class ="calc-btn op"> - </div>
+        <div class ="calc-btn"> 1 </div>
+        <div class ="calc-btn"> 2 </div>
+        <div class ="calc-btn"> 3 </div>
+        <div class ="calc-btn op"> + </div>
+        <div class ="calc-btn zero-btn"> 0 </div>
+        <div class ="calc-btn"> . </div>
+        <div class ="calc-btn op"> = </div>
       </div>
     </div>
 `;
