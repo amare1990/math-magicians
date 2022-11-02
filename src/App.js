a44c5a79ff0156255827c04b7772e3fca4722108
amare1990 <amaremek@gmail.com>
2022-11-02 15:10:00 +0300

    Remove unneccessary code snippts from the App folder

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import parse from 'html-react-parser';
+import Calculator from './components/Calculator';
+import './components/styles.css';
+/* import calcWrapper from './components/Calculator';
+import calcStr from './components/Calculator'; */
+
+const calcObj = new Calculator();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { parse(calcObj.parseStr()) }
     </div>
   );
 }
