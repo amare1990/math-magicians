f62514520280878b1688a834236d3532af48ee39
amare1990 <amaremek@gmail.com>
2022-11-02 17:36:27 +0300

    Modify Calculator.js component to be class-based componenet

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,36 +1,40 @@
-/* const calcHtmlElem = parser.parseFromString(calcStr, 'text/html').body.firstChild;
-calcWrapper.append(calcHtmlElem); */
+import React from 'react';
 
-class Calculator {
-  calcStr = `
-    <div class="calc-wrapper">
-      <input type = "text" class ="inp-display" placeholder = 0>
-      <div class = "grid">
-        <div class ="calc-btn"> AC </div>
-        <div class ="calc-btn"> +/- </div>
-        <div class ="calc-btn"> % </div>
-        <div class ="calc-btn op"> + </div>
-        <div class ="calc-btn"> 7 </div>
-        <div class ="calc-btn"> 8 </div>
-        <div class ="calc-btn"> 9 </div>
-        <div class ="calc-btn op"> * </div>
-        <div class ="calc-btn"> 4 </div>
-        <div class ="calc-btn"> 5 </div>
-        <div class ="calc-btn"> 6 </div>
-        <div class ="calc-btn op"> - </div>
-        <div class ="calc-btn"> 1 </div>
-        <div class ="calc-btn"> 2 </div>
-        <div class ="calc-btn"> 3 </div>
-        <div class ="calc-btn op"> + </div>
-        <div class ="calc-btn zero-btn"> 0 </div>
-        <div class ="calc-btn"> . </div>
-        <div class ="calc-btn op"> = </div>
+class Calculator extends React.Component {
+  calcHtml = (
+    <div className="calc-wrapper">
+      <input type="text" className="inp-display" placeholder={0} />
+      <div className="grid">
+        <div className="calc-btn"> AC </div>
+        <div className="calc-btn"> +/- </div>
+        <div className="calc-btn"> % </div>
+        <div className="calc-btn op"> + </div>
+        <div className="calc-btn"> 7 </div>
+        <div className="calc-btn"> 8 </div>
+        <div className="calc-btn"> 9 </div>
+        <div className="calc-btn op"> * </div>
+        <div className="calc-btn"> 4 </div>
+        <div className="calc-btn"> 5 </div>
+        <div className="calc-btn"> 6 </div>
+        <div className="calc-btn op"> - </div>
+        <div className="calc-btn"> 1 </div>
+        <div className="calc-btn"> 2 </div>
+        <div className="calc-btn"> 3 </div>
+        <div className="calc-btn op"> + </div>
+        <div className="calc-btn zero-btn"> 0 </div>
+        <div className="calc-btn"> . </div>
+        <div className="calc-btn op"> = </div>
       </div>
     </div>
-`;
+  );
 
-  parseStr() {
-    return this.calcStr;
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return this.calcHtml;
   }
 }
 
