62d6544ed1f2197dc08080e1fffe561f58bebadc
amare1990 <amaremek@gmail.com>
2022-11-08 22:24:42 +0300

    Add Home.js component

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import { LoremIpsum } from 'react-lorem-ipsum';
+
+function Home() {
+  return (
+    <>
+      <h2>Welcome to our page! </h2>
+      <LoremIpsum p={2} />
+    </>
+  );
+}
+
+export default Home;
